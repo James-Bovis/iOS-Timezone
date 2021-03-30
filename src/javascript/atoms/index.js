dc778ca739189a022c56554b571c0ab0cd6ad112
James-Bovis <james@butternutbox.com>
2021-03-30 18:00:20 +0100

    Store 'selectedTimeZone' in LocalStorage
    This commit stores the contents of the `selectedTimeZoneState` in the browsers
    localStorage. Each time the `selectedTimeZoneState` is updated, the localStorage
    value is set or updated.
    
    By doing so, the App is able to remember the users last set Timezone and it is
    preserved when they next visit the website.

--- src/javascript/atoms/index.js
+++ src/javascript/atoms/index.js
@@ -34,7 +34,10 @@ export const selectedTimeZone = atom({
   default: {
     value: Intl.DateTimeFormat().resolvedOptions().timeZone,
     label: 'London'
-  }
+  },
+  effects_UNSTABLE: [
+    localStorageEffect('selectedTimeZone')
+  ]
 })
 
 // Selectors
